5e106d7207625e43bcd98b6a3fadddcbb7d3c3a8
Juan Esteban Hoyos <jehz.14@hotmail.com>
2025-03-10 22:44:38 -0500

    Documentacion de lineas del do-while del tipoID

--- login.js
+++ login.js
@@ -24,6 +24,7 @@ function registro(){
     let completoID=0;
     do{
     let tipoId = parseInt(prompt(`Ingresa tu identificación \n CC.1 \n TI.2 \n DNI.3`));//se agrega parseInt para que el valor de tipoId no sea string
+    //Switch para revisar que tipo de identificacion tiene la persona
     switch(tipoId){
         case 1:
         tipoIdent[contRegistro]="C.C";
@@ -38,8 +39,8 @@ function registro(){
             console.log("Dato incorrecto")
             break; 
     }
-    completoID=parseInt(prompt("Desea guardar su respuesta \n 1. Si \n 0. No, repetir"));
-    if(completoID==1){
+    completoID=parseInt(prompt("Desea guardar su respuesta \n 1. Si \n 0. No, repetir"));//Se guarda valor de variable para saber si se repite o no el ciclo
+    if(completoID==1){//Si la conficion es 1. (guardar repuesta), se incrementa 1 en contRegistro para que la proxima vez el dato a guardar dentro del arreglo sea el consecutivo
         contRegistro+=1;
     }
     }while(completoID!=1);
