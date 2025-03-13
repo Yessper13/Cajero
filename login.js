dfb97a2b6ed1ec7d2e8ee12444f8e9715b4cb4d2
YESSPER13 <YESSPER13@gmail.com>
2025-03-12 23:21:58 -0500

    Se agrega campo para ingreso de identificacion

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 let contRegistro=0;
 let tipoIdent=[];
+let identify = [];
 
 let login = parseInt(prompt("\n 1. Iniciar sesion \n 2. Registrar \n Elige respuesta (valida)"));//Inicio programa, solicitando login
 
@@ -11,9 +12,8 @@ switch (login) {
         break;
     case 2:
         registro();//Invoca funcion que solicita parametros de registro
+        console.log("Valor de contRegistro:", contRegistro);
         break;       
-
-    
     default: console.log(`Por favor ingresa una opcion valida`);
     break;
     
@@ -23,34 +23,55 @@ switch (login) {
 function registro(){ 
     let completoID=0;
     do{
-    let tipoId = parseInt(prompt(`Ingresa tu identificación \n CC.1 \n TI.2 \n DNI.3`));//se agrega parseInt para que el valor de tipoId no sea string
-    //Switch para revisar que tipo de identificacion tiene la persona
-    switch(tipoId){
-        case 1:
-        tipoIdent[contRegistro]="C.C";
-        break;
-        case 2:
-        tipoIdent[contRegistro]="T.I";
-        break;
-        case 3:
-        tipoIdent[contRegistro]="DNI";
-        break;  
-        default: 
-            console.log("Dato incorrecto")
-            break; 
-    }
-    completoID=parseInt(prompt("Desea guardar su respuesta \n 1. Si \n 0. No, repetir"));//Se guarda valor de variable para saber si se repite o no el ciclo
-    if(completoID==1){//Si la conficion es 1. (guardar repuesta), se incrementa 1 en contRegistro para que la proxima vez el dato a guardar dentro del arreglo sea el consecutivo
-        contRegistro+=1;
-    }
-    }while(completoID!=1);
+        let tipoId = parseInt(prompt(`Ingresa tu identificación \n CC.1 \n TI.2 \n DNI.3`));//se agrega parseInt para que el valor de tipoId no sea string
+        switch(tipoId){
+            case 1:
+            tipoIdent[contRegistro]="C.C";
+            break;
+            case 2:
+            tipoIdent[contRegistro]="T.I";
+            break;
+            case 3:
+            tipoIdent[contRegistro]="DNI";
+            break;  
+            default: 
+                console.log("Dato incorrecto");
+                break; 
+        }
+     //solo bucle se repite siempre y cuando, el usuario quiera elegir otro tipo de ID 
+    }while(confirmarOpcion()!=1);
+   
+    //Se agrega registro de identificacion
+    let identificacion = 0;
+    do { 
+        identificacion =parseInt(prompt(`Ingresa tu numero de ${tipoIdent[contRegistro]}`));
+        if (identificacion !== null && identificacion !== "" && !isNaN(identificacion)) {
+            if (confirmarOpcion()==1) {
+                identify[contRegistro]=identificacion;
+                console.log("Tipo de identificación ingresado:", tipoIdent[contRegistro]);  
+                console.log("Numero de",tipoIdent[contRegistro],":",identify[contRegistro]);
+                contRegistro+=1;
+            } 
+        }
+        
+        
+    }while(identificacion === "" || identificacion === null || isNaN(identificacion) || confirmarOpcion()!=1);
+        
     
 
-    /* let identificacion = parseInt(prompt(`Ingresa tu numero de ${tipoIdent}`));
+
+/*
     let userReg = prompt(`Crea un usuario de al menos 8 caracteres (ej. PROJAS)`);
+     
+    
     let email = prompt(`Ingresa un correo electronico`);
     let paswordReg = parseInt(prompt(`Ingresa una clave de 4 digitos (ej. 1562)`));
     let confirmarPsw = parseInt(prompt(`Confirma tu clave (ej. 1562)`)); */
+}
+
+function confirmarOpcion() {
+    completoID=parseInt(prompt("Desea guardar su respuesta \n 1. Si \n 0. No, repetir"));
+    return completoID;
 }
 
 //funcion para inicio de sesion de usuario
