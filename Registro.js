378494053a5005d86b2ad6a99fd77e21e5d74b63
BPEREZ18 <BPEREZ@servisoft.com.co>
2025-03-17 09:42:40 -0500

    se agrega funcionalidad de cambio de contraseña

--- Registro.js
+++ Registro.js
@@ -1,25 +1,25 @@
 export { registro };
 export { contraseña };
 import { confirmarOpcion } from "./login.js";
-import { contRegistro } from "./login.js"; 
+import { contRegistro } from "./login.js";
 import { tipoIdent, identify, user, pws, email, saldo } from "./login.js";
 import { account } from "./InicioSesion.js";
-let completoID=0;
+let completoID = 0;
 //funcion para el ingreso de contraseña
-function contraseña(antiguo) {//VALIDAR PORQUE ESTA FUNCION NO DEJA GUARDAR LA CONTRASEÑA DEL CAMBIO
-    let antiguo1=antiguo;
-    let repetir=parseInt(0); 
+function contraseña(antiguo) {
+    let antiguo1 = antiguo;
+    let repetir = parseInt(0);
+
     do {
-        repetir=0;
+        repetir = 0;
         let password = parseInt(prompt(`Ingresa una clave de 4 digitos (ej. 1562)`));
-        
         if (password != null && password != "" && password >= 999 && password < 10000) {
             let confirmPassword = parseInt(prompt(`Confirmar contraseña (ej. 1562))`));
-            if (password === confirmPassword) {
-                if (confirmarOpcion(completoID) == 1) {
-                    if(antiguo1 ==1){
-                        pws[account] = password; 
-                    }else{
+            if (password === confirmPassword) {//verificamos que el usuario si ingrese la contraseña correctamente 
+                if (confirmarOpcion(completoID) == 1) {//confirmacion de guardado de datos
+                    if (antiguo1 == 1) {//se aplica para usuario ya registrados
+                        return pws[account] = password; //se retorna la nueva contraseña a cambiarContraseña en opcionesCuenta.js
+                    } else {//si es para usuarios nuevos
                         pws[contRegistro] = password;
                     }
                 }
@@ -30,86 +30,79 @@ function contraseña(antiguo) {//VALIDAR PORQUE ESTA FUNCION NO DEJA GUARDAR LA 
         } else {
             repetir = 1;
         }
-        
-    }while (repetir !=0);          
+    } while (repetir != 0);
 }
 //funcion para registro de usuario
-function registro(){ 
-    
-    do{
+function registro() {
+
+    do {
         let tipoId = parseInt(prompt(`Ingresa tu identificación \n CC.1 \n TI.2 \n DNI.3`));//se agrega parseInt para que el valor de tipoId no sea string
-        switch(tipoId){
+        switch (tipoId) {
             case 1:
-            tipoIdent[contRegistro]="C.C";
-            break;
+                tipoIdent[contRegistro] = "C.C";
+                break;
             case 2:
-            tipoIdent[contRegistro]="T.I";
-            break;
+                tipoIdent[contRegistro] = "T.I";
+                break;
             case 3:
-            tipoIdent[contRegistro]="DNI";
-            break;  
-            default: 
+                tipoIdent[contRegistro] = "DNI";
+                break;
+            default:
                 console.log("Dato incorrecto");
-                break; 
+                break;
         }
-        
-     //El bucle se repite siempre y cuando, el usuario quiera elegir otro tipo de ID 
-    }while(confirmarOpcion(completoID)!=1);
-   
+        //El bucle se repite siempre y cuando, el usuario quiera elegir otro tipo de ID 
+    } while (confirmarOpcion(completoID) != 1);
+
     //Se agrega campo de registro de identificacion
     let identificacion = 0;
-    let repetir =0;
-    do { 
+    let repetir = 0;
+    do {
         repetir = 0;
-        identificacion =parseInt(prompt(`Ingresa tu numero de ${tipoIdent[contRegistro]}`));
+        identificacion = parseInt(prompt(`Ingresa tu numero de ${tipoIdent[contRegistro]}`));
         if (identificacion !== null && identificacion !== "" && !isNaN(identificacion)) {
-            alert("Identificacion ingresada: "+identificacion)
-            if (confirmarOpcion(completoID)==1) {
-                identify[contRegistro]=identificacion;
-                
-            }else{
-                repetir =1;
+            alert("Identificacion ingresada: " + identificacion)
+            if (confirmarOpcion(completoID) == 1) {
+                identify[contRegistro] = identificacion;
+
+            } else {
+                repetir = 1;
             }
         }
-        
-        
-    }while(identificacion === "" || identificacion === null || isNaN(identificacion) || repetir !=0);
-      
+    } while (identificacion === "" || identificacion === null || isNaN(identificacion) || repetir != 0);
+
     //Se agrega campo de registro de usuario
     do {
         repetir = 0;
         let userReg = prompt(`Ingresa usuario (ej. PROJAS)`);
-        if(userReg != null && userReg != ""){
-            alert("Usuario es: "+userReg)
-            if (confirmarOpcion(completoID)==1) {
-                user[contRegistro]=userReg;
-            }else{
-                repetir =1;
+        if (userReg != null && userReg != "") {
+            alert("Usuario es: " + userReg)
+            if (confirmarOpcion(completoID) == 1) {
+                user[contRegistro] = userReg;
+            } else {
+                repetir = 1;
             }
 
         }
-    } while (repetir !=0);
+    } while (repetir != 0);
 
     //Se agrega campo de registro de email
     do {
         repetir = 0;
         let emailReg = prompt(`Ingresa un correo electronico (ej. projas@example.com)`);
-        alert("Correo ingresado: "+emailReg)
-        if(emailReg != null && emailReg != ""){
-            if (confirmarOpcion(completoID)==1) {
-                email[contRegistro]=emailReg;
-            }else{
-                repetir =1;
+        alert("Correo ingresado: " + emailReg)
+        if (emailReg != null && emailReg != "") {
+            if (confirmarOpcion(completoID) == 1) {
+                email[contRegistro] = emailReg;
+            } else {
+                repetir = 1;
             }
 
         }
-    } while (repetir !=0);
+    } while (repetir != 0);
 
     //Se agrega funcion de registro de contraseña
-        contraseña();
-        saldo[contRegistro]+=0
-        
+    contraseña();
+    saldo[contRegistro] += 0
 
-
-  
 }//Fin de la función Registro
--- opcionesCuenta.js
+++ opcionesCuenta.js
@@ -2,99 +2,98 @@ import { aprobacionLogin,account } from './InicioSesion.js'
 import { saldo,user,pws } from "./login.js";
 import { contraseña } from './Registro.js';
 
-function Opciones() {    
-        function Transferencia(){
-            let loop1=parseInt(1);
-            do{//Loop para iniciar Transferencia
-            
-            let consig=parseInt(prompt("A cual cuenta deseas transferir?"));
-            let chkconsig=parseInt(prompt("Confirma la cuenta a la cual cuenta deseas transferir"));
+function Opciones() {
+    function Transferencia(){
+        let loop1=parseInt(1);
+        do{//Loop para iniciar Transferencia
+        
+        let consig=parseInt(prompt("A cual cuenta deseas transferir?"));
+        let chkconsig=parseInt(prompt("Confirma la cuenta a la cual cuenta deseas transferir"));
 
-            if(consig==chkconsig && consig!=account){
-                let loop2=parseInt();
-                do{//Loop para validar valor a consignar
-                    let valorconsig=parseFloat(prompt("Ingresa el valor a transferir"))
-                    if(valorconsig<=saldo[account] && valorconsig>0 ){
-                        saldo[consig]+=valorconsig;
-                        saldo[account]-=valorconsig;
-                        console.log("La consignacion se ha realizado con exito a la cuenta ",consig,",por un valor de: ",valorconsig)
-                        loop2=0;
-                        loop1=0;        
-                    }
-                    else{
-                        console.log("Valor de transaccion no valida o tienes saldo insuficiente")
-                        loop2=parseInt(prompt("1. para ingresar otro valor \n 0. para salir"))
-                        if(loop2==0){
-                            loop1=0
-                        }
-                    }
-                }while(loop2==1)  
-            }
-            else{loop1 = parseInt(prompt("Cuenta invalida \n 1. para intentar de nuevo \n 0. Salir"));
-            }
-        }while(loop1==1)
-        /* optionsInside=parseInt(prompt("Deseas realizar algo mas? \n 1. si \n 0. no, salir")); //Validacion para iniciar de nuevo el bucle de opciones optionsInside */
-        }
-
-        function Retiro(){
-             let loopR=parseInt(1);
-            do{
-                let retiro=parseInt(prompt("Ingresa el valor a retirar"));
-                alert(saldo[account])
-                if( retiro <= saldo[account] && retiro > 0){
-                    saldo[account]-=retiro;
-                    console.log("El retiro fue de: ",retiro," y el saldo restante es ",saldo[account]);
-                    loopR=0
-                }
-                else{loopR = parseInt(prompt("Cuenta invalida \n 1. para intentar de nuevo \n 0. Salir"));
-                }
-            
-            }while(loopR==1)//Fin de loop para retirar
-        }
-
-        function ConsultaSaldo() {
-            alert("\t consulta de saldo");
-            console.log(`hola ${user[account]} tu sualdo actual es de: ${saldo[account]}`);
-        }
-        let antiguo
-        function CambioContraseña(){
-            let antiguo = parseInt(1);
-            let pws1 = parseInt(prompt(`Ingresa tu contraseña actual`))
-            if(pws[account] === pws1){
-                pws[account] = contraseña(antiguo);
-                
-            }else{
-                alert(`Contraseña incorrecta, intentalo de nuevo`);
-            }
-        }
-
-        function RecargaSaldo(){
-            alert("\t Recarga de saldo");
-            let cuentaRecarga=parseFloat(prompt("Ingrese la cuenta a recargar"));
-            alert("La cuenta a recargar es: "+cuentaRecarga);
-            let chk=parseFloat(prompt("Estas seguro? \n 1. Sí \n 0. No"))
-            if(chk==1){
-                let recarga=parseFloat(prompt("Ingrese el valor a recargar sin puntos ni comas \n Minimo 10.000$ y en multiplos de 10"));
-                if(recarga>=10000 && recarga%10===0){
-                saldo[cuentaRecarga]+=recarga
-                alert("Valor recargado correctamente en la cuenta")
+        if(consig==chkconsig && consig!=account){
+            let loop2=parseInt();
+            do{//Loop para validar valor a consignar
+                let valorconsig=parseFloat(prompt("Ingresa el valor a transferir"))
+                if(valorconsig<=saldo[account] && valorconsig>0 ){
+                    saldo[consig]+=valorconsig;
+                    saldo[account]-=valorconsig;
+                    console.log("La consignacion se ha realizado con exito a la cuenta ",consig,",por un valor de: ",valorconsig)
+                    loop2=0;
+                    loop1=0;        
                 }
                 else{
-                    alert("Valor incorrecto")
+                    console.log("Valor de transaccion no valida o tienes saldo insuficiente")
+                    loop2=parseInt(prompt("1. para ingresar otro valor \n 0. para salir"))
+                    if(loop2==0){
+                        loop1=0
+                    }
                 }
+            }while(loop2==1)  
+        }
+        else{loop1 = parseInt(prompt("Cuenta invalida \n 1. para intentar de nuevo \n 0. Salir"));
+        }
+    }while(loop1==1)
+    /* optionsInside=parseInt(prompt("Deseas realizar algo mas? \n 1. si \n 0. no, salir")); //Validacion para iniciar de nuevo el bucle de opciones optionsInside */
+    }
+
+    function Retiro(){
+            let loopR=parseInt(1);
+        do{
+            let retiro=parseInt(prompt("Ingresa el valor a retirar"));
+            alert(saldo[account])
+            if( retiro <= saldo[account] && retiro > 0){
+                saldo[account]-=retiro;
+                console.log("El retiro fue de: ",retiro," y el saldo restante es ",saldo[account]);
+                loopR=0
+            }
+            else{loopR = parseInt(prompt("Cuenta invalida \n 1. para intentar de nuevo \n 0. Salir"));
+            }
+        
+        }while(loopR==1)//Fin de loop para retirar
+    }
+
+    function ConsultaSaldo() {
+        alert("\t consulta de saldo");
+        console.log(`hola ${user[account]} tu sualdo actual es de: ${saldo[account]}`);
+    }
+
+    function CambioContraseña(){
+        let antiguo = parseInt(1);
+        let pws1 = parseInt(prompt(`Ingresa tu contraseña actual`))//pedimos la contraseña actual al usuario
+        if(pws[account] == pws1){//verificamos de que sea igual a la que esta guardada en el vector
+            pws[account] = contraseña(antiguo);//igualamos pws al return enviado desde la funcion contraseña en registro.js
+        }else{
+            alert(`Contraseña incorrecta, intentalo de nuevo`);
+        }
+    }
+
+    function RecargaSaldo(){
+        alert("\t Recarga de saldo");
+        let cuentaRecarga=parseFloat(prompt("Ingrese la cuenta a recargar"));
+        alert("La cuenta a recargar es: "+cuentaRecarga);
+        let chk=parseFloat(prompt("Estas seguro? \n 1. Sí \n 0. No"))
+        if(chk==1){
+            let recarga=parseFloat(prompt("Ingrese el valor a recargar sin puntos ni comas \n Minimo 10.000$ y en multiplos de 10"));
+            if(recarga>=10000 && recarga%10===0){
+            saldo[cuentaRecarga]+=recarga
+            alert("Valor recargado correctamente en la cuenta")
             }
             else{
-                alert("Intentalo nuevamente")
+                alert("Valor incorrecto")
             }
         }
-        
-        return {
-            Transferencia,
-            Retiro,
-            CambioContraseña,
-            ConsultaSaldo,
-            RecargaSaldo
-        };
+        else{
+            alert("Intentalo nuevamente")
+        }
+    }
+    
+    return {
+        Transferencia,
+        Retiro,
+        CambioContraseña,
+        ConsultaSaldo,
+        RecargaSaldo
+    };
 }
 
 export {Opciones}
